7b22b704f31830cb42bdc8eaf7d5ac5cb46fdafd
Lilly <Sutphinl16@gmail.com>
2025-02-11 13:03:03 -0500

    added links to my favorite colors and my hobbies with basic html in each link

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+
+
+
+export default defineConfig({
+    build: {
+        rollupOptions:{
+            input: {
+                main: resolve(__dirname, "index.html"),
+                Hobbies: resolve(__dirname, "Hobbies/index.html"),
+                Colors: resolve(__dirname, "Colors/index.html"),
+            },
+        },
+    },
+});
