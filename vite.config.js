7a6bb8dd123e393b9e946d593f9d935a6c0a14f0
Lilly <Sutphinl16@gmail.com>
2025-02-11 13:17:54 -0500

    added nojekyll

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-
-
-
 export default defineConfig({
     build: {
+        outDir: "docs",
         rollupOptions:{
             input: {
                 main: resolve(__dirname, "index.html"),
