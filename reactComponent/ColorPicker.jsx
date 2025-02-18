a95bd82008dcf0de9276b6b8a5ece33968a6f201
Lilly <Sutphinl16@gmail.com>
2025-02-18 13:12:30 -0500

    Removed home button from colorpicker at the bottom anddeleted array that wasn't being used

--- reactComponent/ColorPicker.jsx
+++ reactComponent/ColorPicker.jsx
@@ -1,17 +1,5 @@
 import { useState } from "react";
 
-//color data array
-const colorsData = [
-  { id: 1, name: "Red", hex: "#FF0000" },
-  { id: 2, name: "Green", hex: "#00FF00" },
-  { id: 3, name: "Blue", hex: "#0000FF" },
-  { id: 4, name: "Yellow", hex: "#FFFF00" },
-  { id: 5, name: "Purple", hex: "#800080" },
-  { id: 6, name: "Orange", hex: "#FFA500" },
-  { id: 7, name: "Pink", hex: "#FFC0CB" }
-];
-// ColorItem component to display one color
-
 
 
 function ColorPicker() {
